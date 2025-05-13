f489d699e40df8c5c15f033fceaaa2bb3fba0a4b
Wilian <wilian.santos3@outlook.com>
2025-05-13 18:49:28 -0300

    Correções de erro para build

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,10 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true
   },
   images: {
-    domains: ['https://84e6-143-255-126-18.ngrok-free.app']
+    domains: [
+      'https://84e6-143-255-126-18.ngrok-free.app',
+      'http://localhost:8000'
+    ]
   }
 }
 
