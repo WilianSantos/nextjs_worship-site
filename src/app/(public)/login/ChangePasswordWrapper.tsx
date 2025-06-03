29653a7d856128160df0eef2f1c783f621b24927
Wilian <wilian.santos3@outlook.com>
2025-06-03 18:27:35 -0300

    Reformulação de pagina login

---
+++ src/app/(public)/login/ChangePasswordWrapper.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { ChangePasswordTokenForm } from './ChangePasswordToken'
+import { useGetVerifyRegistration } from '@/services/hooks/member/useGetVerifyRegistration'
+import { Loader } from '@/components/Loader'
+
+export default function ChangePasswordWrapper() {
+  const searchParams = useSearchParams()
+  const token = searchParams.get('token')
+  const reset_token = searchParams.get('reset_token') as string
+  const [isValid, setIsValid] = useState(false)
+
+  const { data } = useGetVerifyRegistration({ token })
+
+  useEffect(() => {
+    if (token && data?.data?.valid) {
+      setIsValid(true)
+    }
+  }, [token, data])
+
+  if (reset_token) {
+    return (
+      <div className="min-h-screen flex items-center p-5 justify-center bg-indigo-50">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-lg">
+          <div className="text-center">
+            <h1 className="text-2xl font-bold text-gray-900">
+              Sistema de Gestão do Ministério de Louvor
+            </h1>
+          </div>
+
+          <h2 className="text-xl font-semibold text-center text-orange-600">
+            Atualizar Senha
+          </h2>
+
+          {isValid && <ChangePasswordTokenForm reset_token={reset_token} />}
+        </div>
+      </div>
+    )
+  }
+
+  return <Loader />
+}
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,45 +1,12 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
-
-import { ChangePasswordTokenForm } from './ChangePasswordToken'
-import { useGetVerifyRegistration } from '@/services/hooks/member/useGetVerifyRegistration'
+// app/(public)/login/page.tsx
+import { Suspense } from 'react'
+import ChangePasswordWrapper from './ChangePasswordWrapper'
 import { Loader } from '@/components/Loader'
 
-export default function ChangePassword() {
-  const searchParams = useSearchParams()
-  const token = searchParams.get('token')
-  const reset_token = searchParams.get('reset_token') as string
-  const [isValid, setIsValid] = useState(false)
-
-  const { data } = useGetVerifyRegistration({ token })
-
-  useEffect(() => {
-    if (token && data?.data?.valid) {
-      setIsValid(true)
-    }
-  }, [token, data, setIsValid])
-
-  if (reset_token) {
-    return (
-      <div className="min-h-screen flex items-center p-5 justify-center bg-indigo-50">
-        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-lg">
-          <div className="text-center">
-            <h1 className="text-2xl font-bold text-gray-900">
-              Sistema de Gestão do Ministério de Louvor
-            </h1>
-          </div>
-
-          <h2 className="text-xl font-semibold text-center text-orange-600">
-            Atualizar Senha
-          </h2>
-
-          {isValid && <ChangePasswordTokenForm reset_token={reset_token} />}
-        </div>
-      </div>
-    )
-  }
-
-  return <Loader />
+export default function ChangePasswordPage() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <ChangePasswordWrapper />
+    </Suspense>
+  )
 }
